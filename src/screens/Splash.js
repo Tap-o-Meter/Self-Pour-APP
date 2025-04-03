6aba5bdd329256e775d196f77a6fafe932662bbd
Emmanuel Lastra <elastra@uabc.edu.mx>
2025-04-02 23:17:40 -0700

    Optimzed connection

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -21,110 +21,6 @@ import {
   TouchableOpacity 
 } from 'react-native'
 
-// const Splash = ({navigation, requestDeviceList}) => {
-//   const {loading, error} = config.animations;
-
-//   const animations = [loading, loading, error];
-//   const messages = ['Conectando...', 'Cargando...', 'Error de conexión'];
-
-//   const [index, setIndex] = useState(0);
-//   const [loop, setLoop] = useState(true);
-//   const [timesTried, setTimesTried] = useState(0);
-
-//   useEffect(() => {
-//     checkConnection();
-//     return () => clearTimeout(timeoutId);
-//   }, [timesTried]);
-
-//   let timeoutId;
-
-//   const checkConnection = () => {
-//     socket.connect();
-//     timeoutId = setTimeout(() => {
-//       if (socket.connected) {
-//         setIndex(index + 1);
-//         requestDeviceList(socket);
-//       } else if (timesTried > 4) {
-//         setIndex(2);
-//         setLoop(false);
-//       } else setTimesTried(timesTried + 1);
-//     }, 1500);
-//   };
-
-//   const retry = () => {
-//     setTimesTried(0);
-//     setIndex(0);
-//     setLoop(true);
-
-//     checkConnection();
-//   };
-
-//   return (
-//     <View style={styles.mainContainer}>
-//       <Image
-//         style={styles.logo}
-//         resizeMode="contain"
-//         source={config.images.splash}
-//       />
-
-//       <Text style={styles.status}>{messages[index]}</Text>
-
-//       <LottieView
-//         style={styles.loading}
-//         source={animations[index]}
-//         autoPlay
-//         loop={loop}
-//       />
-
-//       {!loop ? (
-//         <RoundButton
-//           style={styles.retryButton}
-//           eneable={true}
-//           caption={'Reintentar'}
-//           onPress={() => retry()}
-//         />
-//       ) : null}
-//     </View>
-//   );
-// };
-
-// const dispatchToProps = dispatch => {
-//   return {
-//     requestDeviceList: socket => {
-//       dispatch(actions.requestDeviceList(socket));
-//     },
-//   };
-// };
-
-// export default connect(null, dispatchToProps)(Splash);
-
-// const styles = StyleSheet.create({
-//   mainContainer: {
-//     ...globalStyles.mainContainer,
-//     alignItems: 'center',
-//   },
-//   logo: {
-//     width: 350,
-//     height: 350,
-//     marginTop: 120,
-//   },
-//   status: {
-//     ...globalStyles.texts.header2,
-//     marginTop: 70,
-//     textAlign: 'center',
-//     marginBottom: 20,
-//   },
-//   loading: {
-//     width: 100,
-//     height: 100,
-//     backgroundColor: 'transparent',
-//   },
-//   retryButton: {
-//     marginTop: 45,
-//   },
-// });
-
-
 const Splash = ({ navigation, requestDeviceList }) => {
   const { loading, error } = config.animations;
   const animations = [loading, loading, error];
@@ -139,15 +35,15 @@ const Splash = ({ navigation, requestDeviceList }) => {
   const [newUrl, setNewUrl] = useState(null);
 
   useEffect(() => {
-    checkConnection();
+    checkConnection(newUrl);
     return () => clearTimeout(timeoutId);
   }, [timesTried]);
 
   let timeoutId;
 
   const checkConnection = (url) => {
-    const _url = url || newUrl || "http://192.168.1.182:3000";
-    initSocket(_url)
+    // const _url = url || "http://192.168.1.12:3000";
+    initSocket(url)
     timeoutId = setTimeout(() => {
       if (socket?.connected) {
         setIndex(index + 1);
